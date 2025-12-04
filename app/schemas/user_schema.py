22cd20e2d3a638fd7a2f4c499289f1507573750c
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-04 16:49:50 +0100

    AOP-10 create user schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -5,3 +5,24 @@ from typing import Optional
 class UserBase(BaseModel):
     username: str
     email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserSchema(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
