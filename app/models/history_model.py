06bd7a959ee96c2b94e99a582571bab445af00f9
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-08 09:06:23 +0100

    add history model

---
+++ app/models/history_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from ..db.database import Base
+
+
+class HistoryLogs(Base):
+    __tablename__ = "history_results"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    category = Column(String, index=True)
+    summary = Column(String, nullable=False)
+    score = Column(Float, nullable=False)
+    ton = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="history_logs")
