46590bda28af10bf77fa82fb7433edabb2a9d6d0
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-09 12:53:27 +0100

    add title and description to swagger

--- app/main.py
+++ app/main.py
@@ -5,7 +5,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from .api.routers import auth, analyze
 from .config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title="Articles Summary API",
+    description=(
+        "This API provides automatic text analysis using AI models. "
+        "It performs zero-shot classification with HuggingFace, generates structured summaries using Gemini, "
+        "detects sentiment, and stores results in a user-specific history. "
+        "Includes authentication, filtering, and retrieval of past analyses."
+    ),
+)
+
 
 origins = [settings.FRONTEND_URL]
 app.add_middleware(
@@ -22,6 +31,6 @@ app.include_router(auth.router)
 app.include_router(analyze.router)
 
 
-@app.get("/")
+@app.get("/", tags=["Home route"])
 def get_home():
     return {"message": "Hello API"}
