ea28d8a34c9d55c8503f15f6e07701a0c6ad4680
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-12 16:23:41 +0100

    add public endpoint

--- app/api/routers/analyze.py
+++ app/api/routers/analyze.py
@@ -18,6 +18,8 @@ def analyze_text(
     db: Session = Depends(get_db),
 ):
     text = body.text
+    is_public = body.is_public
+
     result = analyzer(text)
 
     if isinstance(result, str):
@@ -27,6 +29,7 @@ def analyze_text(
     score = result["score"]
     summary = result["summary"]
     sentiment = result["sentiment"]
+    title = result["title"]
 
     new_article = HistoryLogs(
         user_id=current_user.id,
@@ -34,6 +37,8 @@ def analyze_text(
         summary=summary,
         score=score,
         sentiment=sentiment,
+        title=title,
+        is_public=is_public,
     )
 
     db.add(new_article)
@@ -56,9 +61,12 @@ def get_user_history(
 def get_filtered_articles(
     category: str | None = None,
     sentiment: str | None = None,
+    current_user: UserSchema = Depends(get_current_user),
     db: Session = Depends(get_db),
+    skip: int = 0,
+    limit: int = 10,
 ):
-    query = db.query(HistoryLogs)
+    query = db.query(HistoryLogs).filter(HistoryLogs.user_id == current_user.id)
 
     if category:
         query = query.filter(HistoryLogs.category == category)
@@ -66,4 +74,21 @@ def get_filtered_articles(
     if sentiment:
         query = query.filter(HistoryLogs.sentiment == sentiment)
 
-    return query.all()
+    return query.offset(skip).limit(limit).all()
+
+
+@router.get("/public", response_model=list[AnalyzeResponse])
+def get_public_articles(
+    db: Session = Depends(get_db),
+    skip: int = 0,
+    limit: int = 20,
+):
+
+    logs = (
+        db.query(HistoryLogs)
+        .filter(HistoryLogs.is_public == True)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+    return logs
